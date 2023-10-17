417ab95c82817bceb49743c1c4135c251b45fb74
Ramiro Esaú Ramírez Alonzo <ramirocell10@hotmail.com>
2023-10-16 20:30:49 -0600

    Update ClasificacionesController.cs
    Se agregan  endpoints faltantes para el CRUD

--- API/FunLandAPI/Controllers/ClasificacionesController.cs
+++ API/FunLandAPI/Controllers/ClasificacionesController.cs
@@ -8,12 +8,70 @@ namespace FunLandAPI.Controllers
     [Route("[controller]/[action]")]
     public class ClasificacionesController : ControllerBase
     {
-     
-        [HttpGet(Name = "Clasificaciones")]
-        public async Task<IActionResult> Get()
+        [HttpPost(Name ="Add")]
+        public async Task<IActionResult> Add(string descripcion)
+        {
+            //TODO: No se si ya jala / Corregir DB y revisar
+            var clasificacionToAdd = await new FunLandContext().Clasificacions.Where(x=> x.Descripcion == descripcion).SingleOrDefaultAsync();
+
+            if(clasificacionToAdd is null)
+            {
+                clasificacionToAdd = new Clasificacion();
+                clasificacionToAdd.Descripcion = descripcion;
+            }
+
+            clasificacionToAdd.Activo = true;
+
+            var newClasificacion = await new FunLandContext().Clasificacions.AddAsync(clasificacionToAdd);
+
+            return Ok(newClasificacion);
+        }
+
+        [HttpGet(Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var clasificacion = await new FunLandContext().Clasificacions.Where(x=> x.IdClasificacion == id).SingleOrDefaultAsync();
+            return Ok(clasificacion);
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public async Task<IActionResult> GetAll()
         {
             var clasificaciones = await new FunLandContext().Clasificacions.ToListAsync();
             return Ok(clasificaciones);
+        }
+
+        [HttpPut(Name = "Update")]
+        public async Task<IActionResult> Update(int id,string descripcion)
+        {
+            using (var ctx = new FunLandContext())
+            {
+                var clasificacionToUpdate = await ctx.Clasificacions.Where(x => x.IdClasificacion == id).SingleOrDefaultAsync();
+
+                if (clasificacionToUpdate is null)
+                    return BadRequest("El registro no existe indicado no existe");
+
+                clasificacionToUpdate.Descripcion = descripcion;
+                ctx.SaveChanges();
+
+                return Ok(clasificacionToUpdate);
+            }
+        }
+
+        [HttpDelete(Name = "Delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            using (var ctx = new FunLandContext())
+            {
+                var clasificacionToDelete = await ctx.Clasificacions.Where(x => x.IdClasificacion == id).SingleOrDefaultAsync();
+
+                if (clasificacionToDelete is null)
+                    return BadRequest("El id indicado no existe");
+
+                clasificacionToDelete.Activo = false;
+                ctx.SaveChanges();
+                return Ok();
+            }
         }
     }
 }
