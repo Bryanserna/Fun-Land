53ce200905e38f2090fe6d9e2abeb21000a4b79e
Bryan <bryanserna1@hotmail.com>
2023-10-18 20:06:22 -0600

    carrito

---
+++ API/FunLandAPI/Controllers/CarritoController.cs
@@ -0,0 +1,19 @@
+﻿using FunLandAPI.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunLandAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]/[Action]")]
+    public class CarritoController : ControllerBase
+    {
+        [HttpPost(Name = "Add")]
+
+        public async Task<IActionResult> Add(string escripcion)
+        {
+            var carritoToAdd = await new FunLandContext().Carritos.Where(x => x.IdUsuario == id).SingleOrDefaultAsync();
+        }
+
+    }
+}
