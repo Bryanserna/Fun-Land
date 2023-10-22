71fc654ef4e846e050e86455b617f0a709456462
Bryan <bryanserna1@hotmail.com>
2023-10-21 22:33:05 -0600

    usuario completo

---
+++ API/FunLandAPI/DTO/UsuarioDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FunLandAPI.DTO
+{
+    public record struct UsuarioDTO
+    {
+        public int Id { get; init; }
+        public required string Nombre { get; set; }
+        public required string Correo { get; set; }
+
+        public required string Password { get; set; }
+        public required bool Activo { get; set; }
+    }
+}
